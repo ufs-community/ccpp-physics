a5981bcffdd66a387da82fce168127691d54523f
Dustin Swales <dustin.swales@noaa.gov>
2024-09-23 20:57:32 +0000

    Some changes and housekeeping to keep Thompson and TEMPO MP distinct, for now

--- physics/MP/Thompson/mp_thompson.F90
+++ physics/MP/Thompson/mp_thompson.F90
@@ -9,10 +9,11 @@ module mp_thompson
       use mpi_f08
       use machine, only : kind_phys
 
-      use module_mp_thompson_params
-      use module_mp_thompson_utils
-      use module_mp_thompson_main
-      use module_mp_thompson, only : thompson_init, mp_gt_driver, thompson_finalize
+      use module_mp_thompson, only : thompson_init, mp_gt_driver, thompson_finalize, calc_effectRad
+      use module_mp_thompson, only : naIN0, naIN1, naCCN0, naCCN1, eps, Nt_c_l, Nt_c_o
+      use module_mp_thompson, only : re_qc_min, re_qc_max, re_qi_min, re_qi_max, re_qs_min, re_qs_max
+
+      use module_mp_thompson_make_number_concentrations, only: make_IceNumber, make_DropletNumber, make_RainNumber
 
       implicit none
 
